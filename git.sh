4f9e2839c9f6a22380f10f069eb9f4b7ec7d479e
Kim Taehyung <33085835+Zerapium@users.noreply.github.com>
2019-12-03 19:04:50 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,10 +1,17 @@
 #/bin/sh
 
+
+
+
+#/bin/sh
+
 # Fetch the newest code
-git fetch origin master
+git pull origin master
 
 # Hard reset
 git reset --hard origin/master
+
+git remote set-head origin master
 
 # Force pull
 git pull origin master --force
