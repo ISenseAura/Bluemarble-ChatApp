9d8b497fd58baf3755f116ecf6340f4bb92ca1f5
Kim Taehyung <33085835+Zerapium@users.noreply.github.com>
2019-12-03 17:43:20 +0530

    Update chatroom.js

--- Commands/chatroom.js
+++ Commands/chatroom.js
@@ -3,7 +3,7 @@ const Tools = require("../plugins/Tools");
 const getEmbedUrl = require("video-embed");
 
 
-functon unescapeHtml(unsafe) {
+function unescapeHtml(unsafe) {
     return unsafe
          .replace(/&amp;/g, "&")
          .replace(/&lt;/g, "<")
